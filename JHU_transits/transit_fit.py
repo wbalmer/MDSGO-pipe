84368066a53de42062ddcf51ee8263a42dab5613
wbalmer <williamobalmer@gmail.com>
2021-10-03 10:11:36 -0400

    updated transit_fit.py script with func to sigma clip and bin data

--- JHU_transits/transit_fit.py
+++ JHU_transits/transit_fit.py
@@ -1,18 +1,34 @@
-# imports
+### imports ###
+# transit fitting suite
 import juliet
+# general data handling
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# BJD time correction tools
 from astropy import coordinates as coord, units as u
 from astropy.time import Time
 
-# user inputs
+# plotting
+import matplotlib.pyplot as plt
+# setting plotting style
+import matplotlib as mpl
+mpl.rcParams['font.sans-serif'] = 'cm'
+plt.rcParams["font.family"] = "sans-serif"   # Fonts
+from matplotlib import rc
+rc('text', usetex=True)
+
+### user inputs ###
 target_name = "TOI-1608"
 T_c = [2458792.459651,.0005086198]
-baseindex = [0,1]
+plot_time = 2459000
+baseindex = [1100,1280]
 P = [2.47272155931859,.00010807506]
 target_coords = ["03 23 12.1412544434", "+33 04 42.181053963"]
 aperture_photometry_file = 'TOI-1608-apphot.xls'
+sigma_clip_factor = 10
+bin_factor = 10
+
+### the code ###
 
 # read in aperture photometry
 apphot = pd.read_csv(aperture_photometry_file, sep='\t')
@@ -40,10 +56,16 @@ forig = f
 f = f/baselinemed
 ferr = (ferr/forig)*f
 
+if sigma_clip_factor is not None:
+    from astropy.stats import sigma_clip
+    f = sigma_clip(f, sigma=sigma_clip_factor, maxiters=1)
+if bin_factor is not None:
+    t, f, ferr = juliet.bin_data(t, f, bin_factor)
+
 # prepare the dictionaries for juliete
 
-times, fluxes, fluxes_error, fwhm = {},{},{},{}
-times['jhu'], fluxes['jhu'], fluxes_error['jhu'], fwhm['jhu'] = t,f,ferr,fwhms
+times, fluxes, fluxes_error = {},{},{},{}
+times['jhu'], fluxes['jhu'], fluxes_error['jhu'] = t,f,ferr
 
 params = ['P_p1','t0_p1','p_p1','b_p1','q1_jhu','q2_jhu','ecc_p1','omega_p1',\
               'rho', 'mdilution_jhu', 'mflux_jhu', 'sigma_w_jhu', 'GP_sigma_jhu', 'GP_rho_jhu']
@@ -69,10 +91,13 @@ dataset = juliet.load(priors=priors, t_lc = times, y_lc = fluxes,
 results = dataset.fit()
 
 # Plot the GP fit and then the detrended + fit transit light curve
+
+# gather the data
 transit_plus_GP_model = results.lc.evaluate('jhu')
 transit_model = results.lc.model['jhu']['deterministic']
 gp_model = results.lc.model['jhu']['GP']
 
+# figure
 fig = plt.figure(constrained_layout=True, figsize=(8,12))
 axd = fig.subplot_mosaic(
     """
@@ -81,17 +106,20 @@ axd = fig.subplot_mosaic(
     """
 )
 
-axd['A'].errorbar(times['jhu'], fluxes['jhu'], fluxes_error['jhu'],fmt='.',alpha=0.4)
-axd['A'].plot(dataset.times_lc['jhu'], transit_plus_GP_model, color='black',zorder=10)
+axd['A'].errorbar(times['jhu']-plot_time, fluxes['jhu'], fluxes_error['jhu'],fmt='.',alpha=0.4)
+axd['A'].plot(dataset.times_lc['jhu']-plot_time, transit_plus_GP_model, color='black',zorder=10)
 
-axd['B'].errorbar(times['jhu'], fluxes['jhu']-gp_model, fluxes_error['jhu'],fmt='.',alpha=0.4, zorder=0, label='detrended lightcurve')
-axd['B'].plot(dataset.times_lc['jhu'], transit_model, color='xkcd:brick',zorder=1, linewidth=3, label='transit model')
-t_bin, y_bin, yerr_bin = juliet.bin_data(times['jhu'], fluxes['jhu']-gp_model, 5)
+axd['B'].errorbar(times['jhu']-plot_time, fluxes['jhu']-gp_model, fluxes_error['jhu'],fmt='.',alpha=0.4, zorder=0, label='detrended lightcurve')
+axd['B'].plot(dataset.times_lc['jhu']-plot_time, transit_model, color='xkcd:brick',zorder=1, linewidth=3, label='transit model')
+t_bin, y_bin, yerr_bin = juliet.bin_data(times['jhu']-plot_time, fluxes['jhu']-gp_model, 5)
 axd['B'].errorbar(t_bin, y_bin, yerr = yerr_bin, fmt = 'o', mfc = 'white', mec = 'black', ecolor = 'black', zorder=2, label='binned data')
 
 fig.suptitle(target_name+', MDSGO', fontsize=16)
-axd['A'].set_title('Observations and GP model fit', fontsize=12)
+axd['A'].set_title(r'Observations and GP model fit with \texttt{Juliete}', fontsize=12)
 axd['B'].set_title('Detrended lightcurve', fontsize=12)
 axd['B'].legend(loc='lower right')
+axd['B'].set_xlabel('Time [BJD-{}]'.format(plot_time))
+axd['A'].set_ylabel('Relative Flux')
+axd['B'].set_ylabel('Relative Flux')
 
-plt.savefig(target_name+'-GP.png', dpi=150)
+plt.savefig(target_name+'-GP.png', dpi=300)
